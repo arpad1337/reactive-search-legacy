be7d5438ddd16cf821e29ae8d821a8ba52939b00
Arpad K. <arpad1337@me.com>
2025-05-21 06:40:56 +0200

    Create search-result.model.js

---
+++ search-result.model.js
@@ -0,0 +1,12 @@
+define([
+    'underscore',
+    'backbone'
+], function (_, Backbone) {
+    'use strict';
+
+    var SearchResultModel = Backbone.Model.extend({
+  
+    });
+
+    return SearchResultModel;
+});
